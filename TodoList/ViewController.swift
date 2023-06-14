83d3d6c1429e2c73032c5aac5eb5debb42bce241
Илья Артёменко <mov.inc.xor@gmail.com>
2023-06-14 15:26:01 +0300

    Initial Commit

---
+++ TodoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by Илья on 14.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
